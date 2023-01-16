0be5915b8f3962b1886e4307d71e4a8ef47a36a3
Gelfeto <gelf.learing@gmail.com>
2023-01-16 20:14:40 +0300

    error fixation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     if x == 0:
         print("0 arguments.")
     elif x == 1:
-        print("1 argument: {}".format(sys.argv[1]))
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(x))
         for i in range(x):
