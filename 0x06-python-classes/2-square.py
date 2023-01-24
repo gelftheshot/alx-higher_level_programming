c223c2402edde22170e10e4650516f053f979973
Gelfeto <gelf.learing@gmail.com>
2023-01-24 12:05:40 +0300

    fixation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+# 2-square.py
+# Gelfeto Gebre <gelf.learing@gmail.com>
+"""Define a class Square."""
+
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size=0):
+        """Initialize a new Square.
 
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
