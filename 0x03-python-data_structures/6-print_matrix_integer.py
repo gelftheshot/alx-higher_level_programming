c8dc6108de806b9a92218ba58b89bdd1dc5c5ff0
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-07 14:15:34 +0300

    enja

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+# 6-print_matrix_integer.py
 
 def print_matrix_integer(matrix=[[]]):
+
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end="")
