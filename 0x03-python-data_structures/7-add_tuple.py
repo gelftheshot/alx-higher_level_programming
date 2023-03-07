13759e48bf1cbe58f30a2241b8e774d33f820965
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-07 14:17:16 +0300

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+# 7-add_tuple.py
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
     list_tup = []
-
     for i in range(2):
         if len(tuple_a) == 0:
             return (tuple_b)
