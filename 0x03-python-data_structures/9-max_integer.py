cd46861dbdca4778e2b78943afa8de2e6795abcc
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-07 14:32:26 +0300

    smalllll

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,7 +7,7 @@ def max_integer(my_list=[]):
         return None
     a = my_list[0]
 
-    for i in range(1, len(my_list)-1):
+    for i in range(1, len(my_list)):
         if my_list[i] > a:
             a = my_list[i]
     return (a)
