3fb3d6bc1906b9ab1274e5302f8f9d9c4b469c28
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-08 10:28:31 +0300

    fixing mistakes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,15 +2,15 @@
 # 10-best_score.py
 
 def best_score(a_dictionary):
-
-    if not isinstance(a_dictionary, dict) or len(a_dictinary) == 0:
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    key = list(a_dictionary.keys())[0]
-    max1 = a_dictionary[key]
+    ret = list(a_dictionary.keys())[0]
+    max1 = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > max1:
+            max1 = v
+            ret = k
+    return (ret)
 
-    for i, j in a_dictionary.items():
-        if j > max1:
-            max1 = j
-            key = i
-    return (key) 
