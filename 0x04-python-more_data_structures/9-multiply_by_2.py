51f75ecd1fa77e9bfa76bfe9534ac1dde6703736
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-08 09:40:45 +0300

    fixing

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,5 @@
 # 9-multiply_by_2.py
 
 def multiply_by_2(a_dictionary):
-    new_dick = {}
 
-    new_dick = list(map(lambda x : a_dictionary[x]*2, a_dictionary])
-    return new_dick
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
