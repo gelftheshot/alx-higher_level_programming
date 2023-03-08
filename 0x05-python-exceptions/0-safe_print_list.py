0e87bb3315501d75e74922d3ebec4d853ac1e6d7
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-08 13:33:43 +0300

    fixing the first one

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     tot = 0
     for i in range(x):
         try:
-            print("{}".formate(my_list[i]), end = "")
+            print("{}".format(my_list[i]), end = "")
             tot = tot + 1
         except IndexError:
             break
