46ae2f85428e969869c4f653c16c80a9d75ef5f3
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-08 09:47:49 +0300

    hha

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,12 +5,12 @@ def best_score(a_dictionary):
 
     if not isinstance(a_dictionary, dict) or len(a_dictinary) == 0:
         return None
-    key = list(a_dictionary())[0]
+
+    key = list(a_dictionary.keys())[0]
     max1 = a_dictionary[key]
 
     for i, j in a_dictionary.items():
         if j > max1:
             max1 = j
             key = i
-    return key
-        
+    return (key) 
