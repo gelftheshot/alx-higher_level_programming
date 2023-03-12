29b3cd418062973b495ef374ad6a36b7fa4c2a99
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-13 00:18:44 +0300

    the first of test driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+
+def add_integer(a, b=98):
+    """take two numbers and return their sum
+        
+        Args:
+            a (int) - is the first number
+            b(int) - is the second number with option 98
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+this is the read me file of 0x07 test driven developemt with python
