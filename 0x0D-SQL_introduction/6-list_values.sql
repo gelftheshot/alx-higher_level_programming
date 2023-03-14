e90a876d8bac78d7c004936fe11266524cd1cbd8
Gelfato Gebre <gelf.learing.@gmail.com>
2023-03-14 19:02:55 +0300

    how it works

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- Lists all rows of the table name called first_table.
+SELECT * FROM `first_table`;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Inserts a new row into the table name called first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
