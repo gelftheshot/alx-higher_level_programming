4ef04b04d76903af3c0b6cd9ac796e14fc3ef548
Gelfato Gebre <gelf.learing@gmai.com>
2023-09-05 13:30:31 +0300

    fixing the first advanced project

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for i in "zYxWvUtSrQpOnMlKjIhGfEdCbA":
-	print({}.format(i), end="")
+cons = 0
+for i in range(122, 96, -1):
+	print("{}".format(chr(i - cons)), end="")
+	if cons == 0:
+		cons = 32
+	else:
+		cons = 0
