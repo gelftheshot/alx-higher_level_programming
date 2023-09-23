8f321b3fa028d12584b62bf71b0e821e122c9619
Gelfato Gebre <gelf.learing@gmai.com>
2023-09-24 00:18:46 +0300

    fixing some errors

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_mat = [x**2 for ele in matrix for x in ele]
+    new_mat = [[x**2 for x in ele] for ele in matrix]
     return new_mat
