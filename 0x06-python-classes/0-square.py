6e84e8952d8affa6a4b1c646da032af1f18e8b84
Gelfato Gebre <gelf.learing@gmai.com>
2023-09-26 13:45:25 +0300

    documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,9 +2,9 @@
 # 0-square.py
 # Gelfato Junga <gelf.learing@gmail.com>
 
-"""a python class that defins a square"""
+"""Define a class Square."""
 
 
 class Square:
-    """a calss that represents a Square"""
+    """A calss that represents a Square"""
     pass
