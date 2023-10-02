e8a532ae324f7d59a109837196560aad938dd2c8
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-02 16:09:07 +0300

    fixing the 9th project

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -92,6 +92,7 @@ class Rectangle():
             return rect_1
         else:
             return rect_2
+
     @classmethod
     def square(cls, size=0):
         """Return a new Rectangle with width and height equal to size.
@@ -99,4 +100,4 @@ class Rectangle():
         Args:
             size (int): equal with and height
         """
-        return (cls(size, size))
+        return (Rectangle(size, size))
