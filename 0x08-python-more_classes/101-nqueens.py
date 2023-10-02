e094055e4c3cd9e5f75fce6994fe0013181c77cc
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-02 22:36:50 +0300

    The N Queen

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import sys
+
+"""a class representing a N queen"""
+
+
+def solve_n_queen(n):
+    """the main method that solves the problem
+    Args:
+        n (int) is the number of rows and clo
+    """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if not isinstance(n, int):
+        print("N must be a number")
+        exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    colomon = set()
+    diagonal1 = set()
+    diagonal2 = set()
+    global result
+    result = []
+
+    board = [["."] * n for i in range(n)]
+
+    def backtracker(row):
+        """this is the function that back track our possible resulets"""
+        if (row == n):
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == "Q":
+                        result.append([i, j])
+            print(result)
+            for i in range(n):
+                result.pop()
+            return
+        for col in range(n):
+            if col in colomon or (row + col) in diagonal1 or (row - col) in diagonal2:
+                continue
+            colomon.add(col)
+            diagonal1.add(col + row)
+            diagonal2.add(row - col)
+            board[row][col] = "Q"
+
+            backtracker(row + 1)
+            colomon.remove(col)
+            diagonal1.remove(col + row)
+            diagonal2.remove(row - col)
+            board[row][col] = "."
+    backtracker(0)
+    
+solve_n_queen(int(sys.argv[1]))
