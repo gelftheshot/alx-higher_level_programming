f52b89852155737929c876f2eec2cf188ef43f56
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-04 00:30:59 +0300

    the 5th one tested well

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 
-"""  a function that prints a text with 2 new lines after each of these characters: ., ? and : """
+"""  a function that prints a text with 2 new lines after each of these textacters: ., ? and : """
 
 def text_indentation(text):
     """ a function starts here
         Args:
             text (str) is a string of text
     """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    delma = ".?:"
-    t = 1
-    for char in text:
-        if (not char in delma) and t:
-            print(char, end="")
-            t = 1
-        elif char in delma:
-            print(char)
-            print("")
-            t = 0
-        else:
-            if char != " ":
-                print(char)
-            t = 1
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
+
