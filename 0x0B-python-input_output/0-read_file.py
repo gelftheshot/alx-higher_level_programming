9e14975c4cfaef1b9070668e89776fdcd513255c
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-10 13:54:19 +0300

    fixing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ def read_file(filename=""):
     """ read from file and return the file contant """
     with open(filename, "r") as file:
         tom = file.read()
-        print(tom, end="")
+        print(tom)
