08f839726ba861541f32799e9ed1e5126e4b4e03
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-10 14:03:46 +0300

    fixing documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
+"""Defines a text file-reading function."""
+
 
 def read_file(filename=""):
-    """ read from file and return the file contant
-        Args:
-            filename (str) - is the name of the file
-    """
-    with open(filename, "r", encoding="utf-8") as file:
-        tom = file.read()
-        print(tom)
+    """Print the contents of the file to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
