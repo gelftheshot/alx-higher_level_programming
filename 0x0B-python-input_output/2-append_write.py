3a48e60f2110ee2cad7c9494e581cf82d2b52e69
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-10 14:10:06 +0300

    makeing 2-append to work

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,11 +2,12 @@
 
 """Defines a function that append to a file"""
 
+
 def append_write(filename="", text=""):
     """ append text at the end of the file
         Args:
             filename(str) - the name of the file
             text(str) - the text to be appened
     """
-    with open(filename, "a", encoding="utf-8"):
-        return (filename.append(text))
+    with open(filename, "a", encoding="utf-8") as file:
+        return (file.write(text))
