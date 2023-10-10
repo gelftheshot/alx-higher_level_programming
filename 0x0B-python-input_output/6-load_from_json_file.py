2dee7e5a9407f36da638c6af891724982dd21fca
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-10 14:20:49 +0300

    making documntation

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,6 +7,9 @@ import json
 
 
 def load_from_json_file(filename):
-
+    """ defing a function that does load from filename 
+    Args:
+        filename - the name of the file
+    """
     with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
