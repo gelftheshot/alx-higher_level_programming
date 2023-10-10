3961965f44a6ec25604386a48126796574298cb1
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-10 20:27:37 +0300

    11-student calss

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,7 +1,7 @@
-
 #!/usr/bin/python3
 
-""" defing a class student """
+
+"""defing a class student """
 
 
 class Student():
@@ -16,14 +16,25 @@ class Student():
         """
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age 
+        self.age = age
+
     def to_json(self, attrs=None):
         """ rettuen the dic represtation of calss """
-        if (isinstance(attrs, list)) and (all(isinstance(li, str) for li in attrs)):
-                new_dic = {}
-                for ele in attrs:
-                    if hasattr(self, ele):
-                        new_dic[ele] = self.__dict__[ele]
-                return new_dic
+        if (isinstance(attrs, list)) and\
+                (all(isinstance(li, str) for li in attrs)):
+            new_dic = {}
+            for ele in attrs:
+                if hasattr(self, ele):
+                    new_dic[ele] = self.__dict__[ele]
+            return new_dic
         else:
             return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """ this will load the att of class from json
+        Args:
+            json - the dictionary represtation of json
+        """
+
+        for key, value in json.items():
+            setattr(self, key, value)
