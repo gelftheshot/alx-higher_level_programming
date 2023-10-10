3413f6809d0e26cb214abe06d42d26e4b8d4eb4e
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-10 14:49:10 +0300

    making pycode

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,18 @@
 """this is just a sys module """
 import sys
 
+
 if __name__ == '__main__':
-    save_to_json = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    ma_list = load_from_json("add_item.json")
+    ma_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
     ma_list = []
 
 for i in range(1, len(sys.argv)):
     ma_list.append(sys.argv[i])
 
-save_to_json(ma_list, "add_item.json")
+save_to_json_file(ma_list, "add_item.json")
