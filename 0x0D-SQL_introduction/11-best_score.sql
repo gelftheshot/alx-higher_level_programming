a75c5a93b79e88d43fc59b9b92704d8765ce508d
Gelfato Gebre <gelf.learing@gmail.com>
2023-11-14 22:03:33 +0300

    fixing comments that contin error

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
--- Lists all records in the table second_table with a score >= 10
+-- List all score greater than 10
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `score` >= 10
--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
---script that updates the score of Bob to 10 in the table second_table.
+-- scri tht updt the scr of bb to ten in the tab sec_tab.
 UPDATE `second_table` SET `score` = 10 WHERE `name` = "Bob";
