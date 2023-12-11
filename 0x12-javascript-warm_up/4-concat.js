ba8393d4636fa0c53585a50497aa81373dc43a01
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-11 21:27:43 +0300

    the 4th one

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let v1 = 'undefined';
+const v2 = ' is ';
+let v3 = 'undefined';
+
+if (process.argv[2]) {
+  v1 = process.argv[2];
+}
+
+if (process.argv[3]) {
+  v3 = process.argv[3];
+}
+
+console.log(v1, v2, v3);
