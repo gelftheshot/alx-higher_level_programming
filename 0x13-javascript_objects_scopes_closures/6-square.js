985c2e311fadeb82a5e9323e5ee9ae96044384b5
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-12 11:24:01 +0300

    adding a method that print a sqare and a square sub class that inherit from rectangle

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let b = '';
+        for (let j = 0; j < this.width; j++) {
+          b += c;
+        }
+        console.log(b);
+      }
+    }
+  }
+};
