c773d28703cdbdfce5211e701648ee34d7aa0bc1
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-12 21:41:53 +0300

    semistandard way of wrting code

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const new_list = list.map((i, index) => i * index);
+const newList = list.map((i, index) => i * index);
 console.log(list);
-console.log(new_list);
+console.log(newList);
