cb8d4afab128645290a0547c4b75b9909bfc6ce8
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-12 22:49:49 +0300

    fixing syntax error

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,4 +2,4 @@
 const fs = require('fs');
 const file1 = fs.readFileSync(process.argv[2], 'utf8');
 const file2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+fs.writeFileSync(process.argv[4], file1 + file2);
