7ef9e838a69ede027ae2facc375c35356773cf89
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-17 00:08:34 +0300

    INNER join

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" mysqldb that use python to show every thing from state
+    data base """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name,\
+                 states.name FROM states INNER \
+                JOIN cities ON cities.state_id=states.id")
+    for city in cur.fetchall():
+        print(city)
