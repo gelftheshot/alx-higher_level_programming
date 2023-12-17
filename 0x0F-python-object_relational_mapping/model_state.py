4e6019e03718518dce6bb0061971a41c2df67897
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-17 11:20:57 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,9 +6,11 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 """ represting a states class """
+
+
 class State(Base):
     """ this is states class that will be connect to my sql
         databse use mysqlalchemy modul """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(128), nullable=False)    
+    name = Column(String(128), nullable=False)
