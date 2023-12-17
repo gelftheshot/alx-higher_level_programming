d8a0ff94937c9bf59913f7311cc6fefd17344725
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-17 11:19:06 +0300

    my first intraction to mysql with mysql alchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+""" represting a states class """
+class State(Base):
+    """ this is states class that will be connect to my sql
+        databse use mysqlalchemy modul """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)    
