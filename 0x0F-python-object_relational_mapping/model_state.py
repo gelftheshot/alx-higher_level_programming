7f2e500cceaa3452a620592f2895f943bebbaf3b
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-17 11:24:27 +0300

    coding type error

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -10,7 +9,13 @@ Base = declarative_base()
 
 class State(Base):
     """ this is states class that will be connect to my sql
-        databse use mysqlalchemy modul """
+        databse use mysqlalchemy modul
+
+
+        __tablename__ (str): The name of the MySQL table to store States.
+        id (sqlalchemy.Integer): The state's id.
+        name (sqlalchemy.String): The state's name.
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
