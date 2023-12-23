a524188da6a5bc401cc773b6a072e0273b81f6f1
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-23 11:20:31 +0300

    requirement

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,5 +14,5 @@ class State(Base):
     name (sqlalchemy.String): The state's name.
     """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
