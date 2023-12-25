eaf7039ad54b371cd1d9d7bb79554e4ac1fbf74d
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-25 11:26:07 +0300

    making it now work when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,16 +4,16 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 """ a script that select every thing in the student order by it' id"""
+if __name__ == "__main__":
+    path = f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
 
-path = f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+    engine = create_engine(path)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-engine = create_engine(path)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    states = session.query(State).order_by(State.id)
 
-states = session.query(State).order_by(State.id)
-
-for state in states:
-    print(str(state.id) + ':', state.name)
-session.close()
+    for state in states:
+        print(str(state.id) + ':', state.name)
+    session.close()
