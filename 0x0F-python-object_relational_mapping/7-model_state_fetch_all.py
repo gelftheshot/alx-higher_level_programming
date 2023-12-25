2b27dea0784238de30b8e43d3e9ed3eba1f9b6a8
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-25 11:29:38 +0300

    making it now work when importeded

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
-""" a script that select every thing in the student order by it' id"""
+""" 
+a script that select every thing in the student order by it' id
+"""
 if __name__ == "__main__":
     path = f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
 
