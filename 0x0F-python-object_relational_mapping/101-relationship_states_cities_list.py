648e02e487b3290c23ddd8ee67e0568f1ecf9db4
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-25 17:39:47 +0300

    printing states along with city

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
+
+if __name__ == "__main__":
+    path = f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+    engine = create_engine(path)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    states = session.query(State)
+
+    for state in states:
+        print(str(state.id) + ':', state.name)
+        cities = session.query(City).where(City.state_id == state.id)
+        for city in cities:
+            print("\t{}: {}".format(city.id, city.name))
