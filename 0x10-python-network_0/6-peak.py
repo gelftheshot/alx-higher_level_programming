91cbf37ee74a4a48f914536351f379b1d2a02db2
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-10 01:06:11 +0300

    finding erros

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,7 +11,7 @@ def find_peak(list_of_integers):
     if (x == 2):
         return max(list_of_integers)
     middle = x//2
-    if (list_of_integers[middle] > list_of_integers[middle+1] and list_of_integers[middle - 1]):
+    if (list_of_integers[middle] > list_of_integers[middle+1] and list_of_integers[middle] > list_of_integers[middle - 1]):
         return list_of_integers[middle]
     if list_of_integers[middle+1] > list_of_integers[middle]:
         return find_peak(list_of_integers[middle:])
