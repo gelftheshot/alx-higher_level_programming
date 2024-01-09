427c15245d1456ef696f5b9f40214c02c7ac3f7b
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-09 22:52:39 +0300

    searcing case sensetive serch

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Write a Bash script that takes in a URL, sends a request to that URL
 
-curl -Is "$1" | grep -Fi "Content-Length" | cut -d' ' -f2
+curl -Is "$1" | grep "Content-Length" | cut -d' ' -f2
