0f5e44424c719dbbcfd9d1aca5d7d42c4c9facf0
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-10 01:12:15 +0300

    fixing pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 """ a code that find the max of an arry in the fastest way """
+
+
 def find_peak(list_of_integers):
     """ finding the max of the array """
     x = len(list_of_integers)
@@ -11,7 +13,8 @@ def find_peak(list_of_integers):
     if (x == 2):
         return max(list_of_integers)
     middle = x//2
-    if (list_of_integers[middle] > list_of_integers[middle+1] and list_of_integers[middle] > list_of_integers[middle - 1]):
+    if (list_of_integers[middle] > list_of_integers[middle+1] and
+            list_of_integers[middle] > list_of_integers[middle - 1]):
         return list_of_integers[middle]
     if list_of_integers[middle+1] > list_of_integers[middle]:
         return find_peak(list_of_integers[middle:])
