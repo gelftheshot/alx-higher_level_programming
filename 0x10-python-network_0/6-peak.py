6b1f6384a26a7ea89931eb5d31eae2d9cb1c2adc
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-10 00:22:36 +0300

    finding max based on recurtion

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,12 @@
 """ a code that find the max of an arry in the fastest way """
 def find_peak(list_of_integers):
     """ finding the max of the array """
-    new_list = sorted(list_of_integers)
-    if len(new_list) > 1:
-        return new_list[-1]
+    if (len(list_of_integers) == 0):
+        return None
+    if (len(list_of_integers) == 1):
+        return list_of_integers[0]
+    a = find_peak(list_of_integers[:len(list_of_integers)//2])
+    b = find_peak(list_of_integers[len(list_of_integers)//2:])
+    if a > b:
+        return a
+    return b
