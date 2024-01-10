d948d5791698302c2554be0e401266ff41756a87
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-10 14:08:14 -0800

    posting email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email address
+sends a POST request to the passed URL"""
+import sys
+import requests
+val = {"email": sys.argv[2]}
+r = requests.post(sys.argv[1], data=val)
+print(r.text)
