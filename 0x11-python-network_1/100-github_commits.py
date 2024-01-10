fee6d958341f792e0a23f513d0d7a3634b8249bb
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-11 02:41:34 +0300

    pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" a python script that returns some api about commit history """
+"""A Python script that returns some API about commit history."""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/" + sys.argv[2] + "/" + sys.argv[1] + "/commits"
+    repo_url = sys.argv[2]
+    repo_name = sys.argv[1]
+    url = f"https://api.github.com/repos/{repo_url}/{repo_name}/commits"
+
     r = requests.get(url)
     rjson = r.json()
 
-    j = 0
-    for i in rjson:
-        if (j < 10):
-            print("{}: {}".format(i['sha'], i['commit']['author']['name']))
-            j += 1
+    for i, item in enumerate(rjson):
+        if i < 10:
+            print(f"{item['sha']}: {item['commit']['author']['name']}")
         else:
             break
-
