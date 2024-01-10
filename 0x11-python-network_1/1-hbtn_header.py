4c6d9e5b38d4b49e166daf88864e597f13504640
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-10 23:20:08 +0300

    returning x served by

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to
+the URL and displays the value of the X-Request-Id"""
+import sys
+import urllib.request
+
+with urllib.request.urlopen(sys.argv[1]) as file:
+    headers = file.headers
+    print(headers["X-Request-Id"])
