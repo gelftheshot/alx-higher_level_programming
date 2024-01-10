f12924846ec283f827483a2f003ddaa773513837
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-10 13:16:27 -0800

    handling http error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL, sends a request
+to the URL and displays the body of the response and hanndle error.
+"""
+import sys
+import urllib.request
+import urllib.parse
+if __name__ == "__main__":
+    try:
+         with urllib.request.urlopen(sys.argv[1]) as file:
+            print(file.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
