3721ce9addfb719275d4e65ce1666d22663da196
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-10 13:20:04 -0800

    pycodestyle

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ import urllib.request
 import urllib.parse
 if __name__ == "__main__":
     try:
-         with urllib.request.urlopen(sys.argv[1]) as file:
+        with urllib.request.urlopen(sys.argv[1]) as file:
             print(file.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
