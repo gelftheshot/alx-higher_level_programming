28f17af2b1c8042364e2dedffa003e5535ab52d8
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-11 02:37:29 +0300

    printing commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a python script that returns some api about commit history """
+import requests
+import sys
+
+url = "https://api.github.com/repos/" + sys.argv[2] + "/" + sys.argv[1] + "/commits"
+r = requests.get(url)
+rjson = r.json()
+
+j = 0
+for i in rjson:
+    if (j < 10):
+        print("{}: {}".format(i['sha'], i['commit']['author']['name']))
+        j += 1
+    else:
+        break
+
