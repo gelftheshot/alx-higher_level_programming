db8ed35f069022e5a0a19ae5078bab3daa6a1788
Leon Gebre <gelf.learing@gmail.com>
2024-02-17 17:11:49 +0300

    number of task computed by user id

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Script that gets the contents of a webpage and stores it in a file.
+const axios = require('axios');
+const fs = require('fs').promises;
+const url = process.argv[2];
+const file = process.argv[3];
+
+axios.get(url, {
+    responseType: 'arraybuffer'
+})
+.then(response => {
+    return fs.writeFile(file, response.data);
+})
+.catch(error => {
+    console.error(error.message);
+});
---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Script that computes the number of tasks completed by user id.
+const axios = require('axios');
+const url = process.argv[2];
+const completed = {};
+
+axios.get(url)
+    .then(response => {
+        const results = response.data;
+        for (const result of results) {
+            if (result.completed === true) {
+                if (completed[result.userId] === undefined) {
+                    completed[result.userId] = 1;
+                } else {
+                    completed[result.userId] += 1;
+                }
+            }
+        }
+        console.log(completed);
+    })
+    .catch(error => {
+        console.error(error.message);
+    });
