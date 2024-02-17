2a1a7e476cad2c15adbb85ccde110ff25fd5bf2e
Leon Gebre <gelf.learing@gmail.com>
2024-02-17 16:15:55 +0300

    sami-standard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that reads and prints the content
 const fs = require('fs');
-const file_path = process.argv[2];
-fs.readFile(file_path, 'utf8', function (err, data) {
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf8', function (err, data) {
   console.log(err || data);
 });
